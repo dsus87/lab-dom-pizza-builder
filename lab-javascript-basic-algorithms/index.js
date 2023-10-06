24b2bb8fe1032edfe918c70cea12fc4aa80d0194
Daniel Susetio <dsusetio87@gmail.com>
2023-10-06 19:47:50 +0200

    Bonus done

--- lab-javascript-basic-algorithms/index.js
+++ lab-javascript-basic-algorithms/index.js
@@ -53,4 +53,24 @@ else{
     console.log("What?! You both have the same name?");
 }
 
-// hello hello 
+// BONUS 1
+
+let longText = "elementum. Nulla eget porta neque. Aliquam molestie neque ac condimentum tempor. Sed vehicula nisi ut nisi porttitor, sed malesuada arcu consectetur. Nulla vestibulum efficitur odio id tempor. Aliquam in tempor ex, eget tempor metus. Nullam rutrum venenatis justo in vehicula. Mauris tristique finibus metus a interdum. Cras vitae ipsum ex. Nam ante sem, fringilla eget pretium eget, interdum vel magna. Donec consequat orci ut laoreet consectetur. Aliquam nisi lacus, pharetra blandit lectus maximus, lacinia tincidunt dolor. Donec commodo efficitur orci, vel blandit erat tincidunt sed. Praesent consectetur lectus a massa venenatis egestas. Morbi sit amet vestibulum nisi. Fusce tincidunt dapibus elit sit amet tempor. Duis pharetra ante vitae eros ullamcorper, id mollis mi hendrerit. Ut viverra nunc venenatis, efficitur turpis vel, semper risus. Suspendisse accumsan arcu iaculis, posuere risus at, commodo metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.In efficitur nisl vel lorem porta, gravida placerat leo ornare. Pellentesque nec lectus dui. Aenean vitae orci vitae risus varius accumsan. Mauris felis mauris, euismod et odio vestibulum, porta lobortis dolor. Quisque pellentesque nisl vitae sapien egestas tristique et at libero. Duis maximus dapibus dui, ac viverra mauris imperdiet et. Quisque imperdiet at quam at posuere. In hac habitasse platea dictumst. Duis lobortis tincidunt libero, et consequat mauris mattis eget. In id quam egestas, rhoncus ante quis, sagittis orci. Phasellus venenatis varius dui, vitae posuere odio gravida eu."
+
+
+let words = longText.split(' ')
+let count = words.length
+
+console.log(count)
+
+let etCount = 0
+
+for (let i=0; i<longText.length;i++) {
+ const twoChars =longText[i] + longText[i + 1] + longText[i + 2]
+
+ if (twoChars === ' et' ){
+    etCount++
+ }
+
+}
+console.log(`${etCount}`)
